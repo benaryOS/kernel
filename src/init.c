9455a3b1487eaa28b50f2e1c49f91b12f75f1a8b
benaryorg <binary@benary.org>
2014-08-16 11:19:01 +0200

    changed init to printk

--- src/init.c
+++ src/init.c
@@ -1,13 +1,8 @@
 #include <constants.h>
 
+extern int printk(const char *,...);
+
 void init(void)
 {
-	char hw[]="Hello World!";
-	int i;
-	char *video=TEXT_BUFFER;
-	for(i=0;hw[i]!='\0';i++)
-	{
-		video[i*2]=hw[i];
-		video[i*2+1]=0x07;
-	}
+	printk("benaryOS booted!\n");
 }
