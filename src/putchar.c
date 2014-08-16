f8de1b161be95a80d4cefbea9935c4ae0314f6f7
benaryorg <binary@benary.org>
2014-08-16 11:20:19 +0200

    putchar now writes in green and not every second characters

--- src/putchar.c
+++ src/putchar.c
@@ -1,12 +1,12 @@
 #include <constants.h>
 
 size_t text_pos=0;
-char text_color=0;
+char text_color=0x0a;
 
 int putchar(int ch)
 {
 	*(TEXT_BUFFER+(text_pos*2))=(char)ch;
 	*(TEXT_BUFFER+(text_pos*2)+1)=text_color;
-	text_pos+=2;
+	text_pos++;
 	return ch;
 }
