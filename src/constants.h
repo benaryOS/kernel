45a6e0322357299d0911d8a09bbeaa0f009c7783
benaryorg <binary@benary.org>
2014-08-16 10:21:25 +0200

    added va_args to header file

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,18 @@
 #ifndef __BENARYOS_CONSTANTS__
 #define __BENARYOS_CONSTANTS__
 
+/*
+	va_args
+*/
+typedef __builtin_va_list       va_list;
+#define va_start(ap, X)         __builtin_va_start(ap, X)
+#define va_arg(ap, type)        __builtin_va_arg(ap, type)
+#define va_end(ap)              __builtin_va_end(ap)
+
+/*
+	buffers (text and later video buffer)
+*/
 #define TEXT_BUFFER ((char *)0xb8000)
+
 
 #endif /** __BENARYOS_CONSTANTS__ **/
