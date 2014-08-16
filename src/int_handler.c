5f511b480ecc217abd9abc0f9fbaf6869198eabb
benaryorg <binary@benary.org>
2014-08-16 14:58:15 +0200

    interrupt handler now returns and sends EOI

--- src/int_handler.c
+++ src/int_handler.c
@@ -1,10 +1,22 @@
 #include <constants.h>
 
 extern int printk(const char *,...);
+extern void outb(uint16_t,uint8_t);
 
 struct cpu_state *int_handler(struct cpu_state *cpu)
 {
 	printk("interrupt(%d)\n",cpu->intr);
-	while(1);
+
+	//send end of interrupt (hardware interrupts)
+	if(cpu->intr>=0x20&&cpu->intr<=0x2f)
+	{
+		if(cpu->intr>=0x28)
+		{
+			outb(0xa0,0x20);
+		}
+		outb(0x20,0x20);
+	}
+
+	//while(1);
 	return cpu;
 }
