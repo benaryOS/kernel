ea95dffde28e08ea64b09c3b6032ab2f869b1d81
benaryorg <binary@benary.org>
2014-08-16 11:23:11 +0200

    putchar now supports \r an \n

--- src/putchar.c
+++ src/putchar.c
@@ -5,8 +5,19 @@ char text_color=0x0a;
 
 int putchar(int ch)
 {
-	*(TEXT_BUFFER+(text_pos*2))=(char)ch;
-	*(TEXT_BUFFER+(text_pos*2)+1)=text_color;
-	text_pos++;
+	switch(ch)
+	{
+		case '\r':
+			text_pos-=text_pos%TEXT_WIDTH;
+			break;
+		case '\n':
+			text_pos+=TEXT_WIDTH-(text_pos%TEXT_WIDTH);
+			break;
+		default:
+			*(TEXT_BUFFER+(text_pos*2))=(char)ch;
+			*(TEXT_BUFFER+(text_pos*2)+1)=text_color;
+			text_pos++;
+			break;
+	}
 	return ch;
 }
