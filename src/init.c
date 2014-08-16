8b94420718ef3673799931955a3375f65a4a9749
benaryorg <binary@benary.org>
2014-08-16 11:34:37 +0200

    init now clears the screen

--- src/init.c
+++ src/init.c
@@ -1,8 +1,17 @@
 #include <constants.h>
 
 extern int printk(const char *,...);
+extern int putchar(int);
+extern size_t text_pos;
 
 void init(void)
 {
+	size_t i;
+	for(i=0;i<TEXT_HEIGHT*TEXT_WIDTH;i++)
+	{
+		putchar(' ');
+	}
+	text_pos=0;
+	
 	printk("benaryOS booted!\n");
 }
