2eeabe6a63d8d0b4e8db1d864e329846ceff65bd
benaryorg <binary@benary.org>
2014-09-21 16:47:35 +0200

    added scheduling

--- src/init.c
+++ src/init.c
@@ -10,6 +10,26 @@ extern void pit_init(int);
 extern void idt_init(void);
 extern void pmm_init(struct multiboot *);
 
+extern struct task *task_create_kernel(void *);
+extern struct task *task_create_user(void *);
+
+extern void task_add(struct task *);
+
+void t1(void)
+{
+	while(1)
+	{
+		putchar('a');
+	}
+}
+void t2(void)
+{
+	while(1)
+	{
+		putchar('b');
+	}
+}
+
 void init(struct multiboot *mb)
 {
 	size_t i;
@@ -40,6 +60,12 @@ void init(struct multiboot *mb)
 
 	printk("initialise IDT\n");
 	idt_init();
+
+	struct task *t;
+	t=task_create_kernel(t1);
+	task_add(t);
+	t=task_create_kernel(t2);
+	task_add(t);
 
 	asm volatile("sti");
 
--- src/scheduler.c
+++ src/scheduler.c
@@ -5,18 +5,42 @@ extern size_t printk(const char *,...);
 extern void tss_entry_set(uint32_t,uint32_t);
 
 static struct task *current_task=0;
+static struct task *first_task=0;
+
+void task_add(struct task *t)
+{
+	if(first_task)
+	{
+		t->next=first_task->next;
+		first_task->next=t;
+	}
+	else
+	{
+		t->next=t;
+		first_task=t;
+	}
+}
 
 struct cpu_state *schedule(struct cpu_state *cpu)
 {
 	if(current_task)
 	{
+		current_task->cpu=cpu;
 		current_task=current_task->next;
 		printk("switching to %d\n",current_task->pid);
 		cpu=current_task->cpu;
 	}
 	else
 	{
-		printk("no tasks\n");
+		if(first_task)
+		{
+			current_task=first_task;
+			cpu=current_task->cpu;
+		}
+		else
+		{
+			printk("no tasks\n");
+		}
 	}
 	tss_entry_set(1,(uint32_t)cpu);
 	return cpu;
