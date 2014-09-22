4cd7f91da66cf002350a538483ad969f357902af
benaryorg <binary@benary.org>
2014-09-22 09:46:07 +0200

    using now a page context

--- src/paging.c
+++ src/paging.c
@@ -1,9 +1,17 @@
 #include <constants.h>
 
+static struct page_context kernel_ctx;
+
+void paging_context_activate(struct page_context *ctx)
+{
+	asm volatile("mov %0, %%cr3" : : "r" (&ctx->directory));
+}
+
 void paging_init(void)
 {
-	uint32_t cr0;
+	paging_context_activate(&kernel_ctx);
 
+	uint32_t cr0;
 	asm volatile("mov %%cr0, %0" : "=r" (cr0));
 	cr0 |= (1 << 31);
 	asm volatile("mov %0, %%cr0" : : "r" (cr0));
