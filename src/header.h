6b18f5dbbd5211a3929573d057fca31eb0038792
benaryorg <binary@benary.org>
2015-05-16 00:51:09 +0200

    add comments and EOF

--- src/header.h
+++ src/header.h
@@ -5,6 +5,7 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdarg.h>
 
 #include <macros.h>
 
@@ -17,13 +18,20 @@ struct mb_header
 
 /* CONSTANTS */
 
+#define EOF (-1)
+
 #define TEXT ((void *)0xb8000)
 #define TEXT_WIDTH (80)
 #define TEXT_HEIGHT (40)
 
 /* FUNCTIONS */
 
+//main
+
 void main(struct mb_header *);
+
+//textio
+
 void clear(void);
 
 #endif /* HEADER */
