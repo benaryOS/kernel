d3e160bc5d290354d43f45851050f7684537d635
benaryorg <binary@benary.org>
2015-05-15 18:15:09 +0200

    use header

--- src/clear.c
+++ src/clear.c
@@ -1,15 +1,16 @@
+#include <header.h>
+
 /*
  * clears the screen
  */
 void clear(void)
 {
-	//FIXME: use a header
-	short int *vid=(void *)0xb8000;
+	short int *txt=TEXTBUFFER;
 	int i;
 	for(i=0;i<80*40;i++)
 	{
 		//set to zero
-		vid[i]=0;
+		txt[i]=0;
 	}
 }
 
---
+++ src/header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER
+#define HEADER
+
+/* CONSTANTS */
+
+#define TEXTBUFFER ((void *)0xb8000)
+
+/* FUNCTIONS */
+
+void clear(void);
+
+#endif /* HEADER */
+
