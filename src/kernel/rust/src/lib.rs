7c80000c63efb487522b05d341221c9ef67ab27a
benaryorg <binary@benary.org>
2016-05-20 23:17:55 +0200

    clear console and print status
    Signed-off-by: benaryorg <binary@benary.org>

--- src/kernel/rust/src/lib.rs
+++ src/kernel/rust/src/lib.rs
@@ -1,15 +1,27 @@
 #![no_std]
-
-#![crate_name = "rust"]
-#![crate_type = "lib"]
-
 #![feature(lang_items)]
 
+const CONSOLE_TEXT_PTR: *mut [u8;2] = 0xb8000 as *mut [u8;2];
+const CONSOLE_TEXT_WIDTH: usize = 80;
+const CONSOLE_TEXT_HEIGTH: usize = 25;
+
 #[no_mangle]
-pub extern "C" fn kernel_main() -> !
+pub extern "C" fn kernel_main()
 {
-	loop
+	let mut con = unsafe { core::slice::from_raw_parts_mut(CONSOLE_TEXT_PTR,CONSOLE_TEXT_WIDTH*CONSOLE_TEXT_HEIGTH) };
+
+	for mut arr in con.iter_mut()
 	{
+		arr[0] = ' ' as u8;
+		arr[1] = 10;
+	}
+
+	for (i,ch) in "System running!".chars().map(|ch|ch as u8).enumerate()
+	{
+		if let Some(dat) = con.get_mut(i)
+		{
+			dat[0] = ch;
+		}
 	}
 }
 
